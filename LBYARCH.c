2f1704c5ee49f01b27cb7fd42f3b25a66e050df8
Kailu Baradas <jankailubaradas@gmail.com>
2024-11-29 10:55:58 +0800

    fixed calculation algo on this
    this should be final na

--- LBYARCH.c
+++ LBYARCH.c
@@ -2,6 +2,8 @@
    #include <stdlib.h>
    #include <time.h>
 
+   #define BILLION 1000000000.0
+
    float dot_product_c(const float* A, const float* B, int n) {
        float sdot = 0.0;
        for (int i = 0; i < n; i++) {
@@ -22,11 +24,22 @@
            B[i] = (float) rand() / RAND_MAX;
        }
 
-       clock_t start = clock();
-       float result = dot_product_c(A, B, n);
-       clock_t end = clock();
+       // Repeat the computation to get more stable timing
+       int iterations = 1000;
+       struct timespec start, end;
+       clock_gettime(CLOCK_MONOTONIC, &start);
+       float result = 0.0;
+
+       for (int i = 0; i < iterations; i++) {
+           result = dot_product_c(A, B, n);
+       }
+
+       clock_gettime(CLOCK_MONOTONIC, &end);
+       double time_taken = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / BILLION;
+       double average_time = time_taken / iterations;
+
        printf("Dot Product (C version): %f\n", result);
-       printf("Execution time: %lf seconds\n", (double)(end - start) / CLOCKS_PER_SEC);
+       printf("Average execution time: %lf seconds\n", average_time);
 
        free(A);
        free(B);
